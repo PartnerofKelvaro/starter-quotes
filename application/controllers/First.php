601341a4280f5736ed74bedca73fedec09854c02
Kelvaro <blizzardkang@gmail.com>
2016-01-30 21:10:17 -0800

    Link 3

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -24,7 +24,7 @@ class First extends Application {
 	}
         
       	function zzz()
-	{
+	{   
 		$this->data['pagebody'] = 'justone';	// this is the view we want shown
                 $record = $this->quotes->get(1);
                 $this->data = array_merge($this->data, $record);
